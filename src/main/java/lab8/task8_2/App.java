1fd1500f853a42ae21e77d4c94372622891cae23
Sergii Lisnychyi <sergiilisnychyi@slisnycsmacbook.kpn>
2022-05-29 13:49:55 +0200

    updated task8_2

--- src/main/java/lab8/task8_2/App.java
+++ src/main/java/lab8/task8_2/App.java
@@ -1,27 +1,44 @@
 package lab8.task8_2;
 
 import java.util.Arrays;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class App {
+    private static final ForkJoinPool FORK_JOIN_POOL = new ForkJoinPool();
 
     public static void main(String[] args) {
-        int[] values = generateElems(1_000_000);
+        int[] values = generateElems(5);
         System.out.println("Values = " + Arrays.toString(values));
         int sum = calculateSum(values, 0, values.length - 1);
         System.out.println("Sum = " + sum);
     }
 
     private static int calculateSum(int[] values, int i, int j) {
-        if(j - i <= 2) {
+        if (j - i <= 2) {
             int sum = 0;
-            while (i < j){
+            while (i < j) {
                 sum += values[i++];
             }
             return sum;
         }
         int mid = ((j - i) / 2) + i;
-        return calculateSum(values, i, mid) + calculateSum(values, mid, j);
+        int finalI = i;
+        return FORK_JOIN_POOL.invokeAll(List.of(
+                () -> calculateSum(values, finalI, mid),
+                (Callable<Integer>) () -> calculateSum(values, mid, j)
+        )).stream()
+                .mapToInt(App::execute)
+                .sum();
+    }
+
+    private static Integer execute(Future<Integer> e) {
+        try {
+            return e.get();
+        } catch (InterruptedException | ExecutionException interruptedException) {
+            interruptedException.printStackTrace();
+            throw new IllegalStateException();
+        }
     }
 
 
